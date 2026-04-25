5104f8567b06e8f8a9497326f27c893e5ef5af9d
engez1034-hash <engez1034@gmail.com>
2026-04-25 16:16:22 +0300

    Update next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
   },
   transpilePackages: [
     '@trakyahaber/database',
